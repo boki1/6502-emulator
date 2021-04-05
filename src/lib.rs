b618e890b72f84b3d9f2b511bca238d42cf2e952
boki1 <kristoimenov@gmail.com>
2021-04-06 00:30:52 +0300

    Add getters/setters

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
+#![allow(non_snake_case)]
+
+#[macro_use]
+extern crate getset;
+
 mod mos6502;
--- src/mos6502.rs
+++ src/mos6502.rs
@@ -1,3 +1,7 @@
+#![feature(concat_idents)]
+
+use getset::{CopyGetters, Getters, MutGetters, Setters};
+
 /// This structure represents the registers each MOS 6502 has.
 /// They include one 8-bit accumulator register (A), two 8-bit
 /// index registers (X and Y), 7 1-bit processor status flag
@@ -15,9 +19,44 @@
 /// ||++------ No CPU effect, see: the B flag\
 /// |+-------- Overflow\
 /// +--------- Negative
+///
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct RegisterSet {
+/// **bit!** and **bit_setter!** - Generate a getter and a setter for a concrete bit of a field.
+///
+/// Example:
+/// ```
+/// #[inline]
+/// fn carry(&self) -> bool {
+///     (self.status & 1 << 7)) != 0;
+/// }
+///
+/// #[inline]
+/// fn set_carry(&self, value: bool) -> bool {
+///     self.status = (self.status & !(1 << 7)) | ((value as u8) << 7);
+/// }
+/// ```
+
+macro_rules! bit_setter {
+    ($n: expr, $name: ident) => {
+        #[inline]
+        fn $name(&mut self, value: bool) {
+            self.status = (self.status & !(1 << $n)) | ((value as u8) << $n);
+        }
+    };
+}
+
+macro_rules! bit {
+    ($n: expr, $name: ident) => {
+        #[inline]
+        fn $name(&self) -> bool {
+            (self.status & (1 << $n)) != 0
+        }
+    };
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Getters, CopyGetters, Setters, MutGetters)]
+#[getset(get_copy = "pub", set = "pub", get_mut = "pub")]
+pub struct RegisterSet {
     accumulator: i8,
     x_index: i8,
     y_index: i8,
@@ -37,21 +76,48 @@ impl RegisterSet {
             status: 0x24,
         }
     }
+
+    bit!(0, carry);
+    bit!(1, zero);
+    bit!(2, irq_disabled);
+    bit!(3, decimal_mode);
+    bit!(4, brk);
+    bit!(5, unused);
+    bit!(6, overflowed);
+    bit!(7, negative);
+
+    bit_setter!(0, set_carry);
+    bit_setter!(1, set_zero);
+    bit_setter!(2, set_irq_disabled);
+    bit_setter!(3, set_decimal_mode);
+    bit_setter!(4, set_brk);
+    bit_setter!(5, set_unused);
+    bit_setter!(6, set_overflowed);
+    bit_setter!(7, set_negative);
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Timings {
+impl Default for RegisterSet {
+    fn default() -> Self {
+        RegisterSet::new()
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Getters, CopyGetters, Setters, MutGetters, Default)]
+#[getset(get_copy = "pub", set = "pub", get_mut = "pub")]
+pub struct Timings {
     elapsed: u64,
     residual: u8,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct InterruptHandling {
+#[derive(Debug, Copy, Clone, PartialEq, Getters, CopyGetters, Setters, MutGetters, Default)]
+#[getset(get_copy = "pub", set = "pub", get_mut = "pub")]
+pub struct InterruptHandling {
     pending_nmi: bool,
     pending_irq: bool,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Getters, CopyGetters, Setters, MutGetters)]
+#[getset(get_copy = "pub", set = "pub", get_mut = "pub")]
 pub struct Cpu {
     regset: RegisterSet,
     time: Timings,
@@ -74,8 +140,46 @@ impl Cpu {
     }
 }
 
+impl Default for Cpu {
+    fn default() -> Self {
+        Cpu::new()
+    }
+}
+
+pub trait CommunicationInterface {
+    fn read(&self, address: u16) -> u8;
+    fn write(&self, address: u16, data: u8);
+}
+
+/// Tests
+
 #[test]
-fn create_cpu() {
+fn test__create_cpu() {
     let _ = Cpu::new();
     assert!(true);
+}
+
+#[test]
+fn test__getters_fields() {
+    let regset = RegisterSet::new();
+    assert_eq!(regset.accumulator(), 0);
+    assert_eq!(regset.stk_ptr(), 0xfd);
+}
+
+#[test]
+fn test__setters_fields() {
+    let mut regset = RegisterSet::new();
+    regset.set_accumulator(1);
+    assert_eq!(regset.accumulator(), 1);
+}
+
+#[test]
+fn test__getters_and_setters_bits() {
+    let mut regset = RegisterSet::default();
+    let carry_is_set: bool = regset.carry();
+    assert_eq!(carry_is_set, false);
+
+    regset.set_carry(true);
+    let carry_is_set: bool = regset.carry();
+    assert_eq!(carry_is_set, true);
 }
